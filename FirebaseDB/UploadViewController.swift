dd1b240dc77d9f8f0760f80217ab67d12fc92a9f
prettyturtle <unifconti@gmail.com>
2022-03-12 00:31:04 +0900

    Firebase DB 프로젝트 생성, SPM으로 SnapKit, RxSwift, Firebase 설치

---
+++ FirebaseDB/UploadViewController.swift
@@ -0,0 +1,14 @@
+//
+//  UploadViewController.swift
+//  FirebaseDB
+//
+//  Created by yc on 2022/03/12.
+//
+
+import UIKit
+
+class UploadViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
